041bcaa43ece9aff510e30f8667179429bb41520
bahador <noor.bahadory786@gmail.com>
2024-02-01 02:41:29 +0000

    fetched shows

--- script.js
+++ script.js
@@ -5,11 +5,44 @@ const episodeNumber = document.querySelector(".episodes-numbers");
 const inputWrapper = document.querySelector(".input-wrapper");
 inputWrapper.appendChild(episodeNumber);
 let episodeSelect = document.getElementById("episodeSelect");
-const homePagBtn = document.querySelector(".btn");
-homePagBtn.addEventListener("click", function () {
-  clearRender();
-  makePageForEpisodes(btnEpi);
+const homePageBtn = document.querySelector(".btn");
+const dropdown = document.getElementById("showDropdown");
+
+// fetch("https://api.tvmaze.com/shows")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.forEach((show) => {
+//       const option = document.createElement("option");
+//       option.value = show.id;
+//       option.textContent = `ID-Number ${show.id} : ${show.name}`;
+//       dropdown.appendChild(option);
+//       //console.log(data);
+//     });
+//   })
+//   .catch((error) => console.error("Error fetching shows:", error));
+
+// dropdown.addEventListener("change", function () {
+//   clearRender();
+//   makePageForEpisodesShows(dropdown);
+
+//   //console.log("Dropdown changed:", dropdown.value);
+// });
+
+//home page event listener,
+homePageBtn.addEventListener("click", function () {
+  getAllEpisodes().then((btnEpi) => {
+    clearRender();
+    makePageForEpisodes(btnEpi);
+  });
 });
+
+async function getAllEpisodes() {
+  const movieUrl = "https://api.tvmaze.com/shows/82/episodes";
+  const allEpisodes = await fetch(movieUrl).then((response) => {
+    return response.json();
+  });
+  return allEpisodes;
+}
 
 let btnEpi = getAllEpisodes();
 let totalEpisodes = 0;
@@ -28,7 +61,7 @@ function getEpisodeTitleAndNumber() {
     const allEpisodes = data;
     allEpisodes.forEach((episode) => {
       let option = document.createElement("option");
-      option.text = `Season ${episode.season} Episode ${episode.number} : ${episode.name}`;
+      option.text = ` Episode ${episode.id} : ${episode.name}`;
       // option.text = episode.name;
       episodeSelect.appendChild(option);
     });
@@ -37,7 +70,7 @@ function getEpisodeTitleAndNumber() {
   episodeSelect.addEventListener("change", async (epi) => {
     const allEpisodes = await getAllEpisodes();
     const colon = epi.target.value.indexOf(":");
-    const episodeName = epi.target.value.slice((0, colon + 2));
+    const episodeName = epi.target.value.slice(colon + 2);
     const filEpi = allEpisodes.filter(
       (episode) => episode.name === episodeName
     );
@@ -63,7 +96,7 @@ async function filterEpisodesBySearchTerm() {
 }
 
 async function getAllEpisodes() {
-  const movieUrl = "https://api.tvmaze.com/shows/82/episodes";
+  const movieUrl = "https://api.tvmaze.com/shows";
   const allEpisodes = await fetch(movieUrl).then((response) => {
     return response.json();
   });
@@ -126,3 +159,26 @@ function clearRender() {
 }
 
 window.onload = setup;
+
+function makePageForEpisodesShows(episodeList) {
+  for (let i = 0; i < episodeList.length; i++) {
+    const card = createClassAndElement("div", "title-div");
+    rootElem.appendChild(card);
+
+    const seasonName = episodeList[i].name;
+    const seasonNumber = episodeList[i].id;
+    //const convertSeasonNumberToStr = String(seasonNumber).padStart(2, "0");
+    //const convertSeasonToStr = String(episodeList[i].season).padStart(2, "0");
+    ///const episodeCode = `${seasonName} S${convertSeasonToStr}-E${convertSeasonNumberToStr}`;
+    const season = createClassAndElement("h1", "title");
+    season.textContent = episodeCode;
+    card.appendChild(season);
+    const imgElement = createClassAndElement("img");
+    imgElement.setAttribute("src", episodeList[i].url.image.medium);
+    card.appendChild(imgElement);
+
+    const summary = createClassAndElement("p");
+    summary.innerHTML = episodeList[i].summary;
+    card.appendChild(summary);
+  }
+}
